bcc2f0b0d6364c9edc35d4b1a510363fdaab397d
ilgar222 <ilqar6117@gmail.com>
2023-03-07 20:04:29 +0400

    trim

---
+++ src/main/java/lesson06/DynamicInputApp.java
@@ -0,0 +1,21 @@
+package lesson06;
+
+import java.util.Scanner;
+
+public class DynamicInputApp {
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    String word = scan.nextLine();
+
+        System.out.println(word.trim());
+       // System.out.println(word.toUpperCase());
+      //  System.out.println(word.toLowerCase());
+
+
+
+        //System.out.printf("a:%.5f",num);
+
+
+    }
+}
