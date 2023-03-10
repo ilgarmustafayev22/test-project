1ad547e6222c9d5f6945bea2fd7bfc119ad8f7c3
ilgar222 <ilqar6117@gmail.com>
2023-03-10 19:47:08 +0400

    Task

--- src/main/java/lesson08/WhileLoopApp.java
+++ src/main/java/lesson08/WhileLoopApp.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 
 public class WhileLoopApp {
     public static void main(String[] args) {
+        // 1. Input
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
+
+        //2. Process
         int sum = 0;
-        int i = 0;
-        while (i < 7) {
-            int remainder = num % 10;
-            num = num / 10;
-            sum = sum + remainder;
-            i++;
+        int prod = 1;
+        int count = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+            count++;
+            sum += rem;
+            prod *= rem;
         }
-        System.out.println(sum);
+
+        // 3. Output
+        System.out.printf("Sum of digits is: %d\n", sum);
+        System.out.printf("Prod of digits is: %d\n", prod);
+        System.out.printf("Count of digits is: %d\n", count);
     }
 }
