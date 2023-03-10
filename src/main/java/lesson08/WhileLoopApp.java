4232981beeb289642d0398a6fe4b471323a65164
ilgar222 <ilqar6117@gmail.com>
2023-03-10 19:27:50 +0400

    Task

---
+++ src/main/java/lesson08/WhileLoopApp.java
@@ -0,0 +1,19 @@
+package lesson08;
+
+import java.util.Scanner;
+
+public class WhileLoopApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+         int sum = 0;
+        int i = 0;
+        while (i < 7) {
+            int remainder = num % 10;
+            num = num / 10;
+            sum = sum + remainder;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
