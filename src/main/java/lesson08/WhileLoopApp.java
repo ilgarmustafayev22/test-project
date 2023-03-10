1fda22e0b816ccf89799945426c3d49d35658060
ilgar222 <ilqar6117@gmail.com>
2023-03-10 19:28:30 +0400

    Task

--- src/main/java/lesson08/WhileLoopApp.java
+++ src/main/java/lesson08/WhileLoopApp.java
@@ -6,7 +6,7 @@ public class WhileLoopApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
-         int sum = 0;
+        int sum = 0;
         int i = 0;
         while (i < 7) {
             int remainder = num % 10;
