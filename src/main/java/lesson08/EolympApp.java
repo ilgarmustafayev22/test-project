67603adae099b0e2d14ad9ffcc2b1fd588192bd9
ilgar222 <ilqar6117@gmail.com>
2023-03-13 20:17:48 +0400

    Task

---
+++ src/main/java/lesson08/EolympApp.java
@@ -0,0 +1,19 @@
+package lesson08;
+
+import java.util.Scanner;
+
+public class EolympApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int prod = 1;
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+            prod *= rem;
+        }
+        System.out.println(prod);
+
+
+    }
+}
