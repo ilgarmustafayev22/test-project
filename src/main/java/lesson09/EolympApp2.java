d5740514b791c2016560bf3e7285d62e0f95975c
ilgar222 <ilqar6117@gmail.com>
2023-03-13 23:40:55 +0400

    Task

--- src/main/java/lesson09/EolympApp2.java
+++ src/main/java/lesson09/EolympApp2.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class EolympApp2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int month = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
 
-        if (month >= 3 && month <= 5) {
-            System.out.println("Spring");
-        } else if (month >= 6 && month <= 8) {
-            System.out.println("Summer");
-        } else if (month >= 9 && month <= 11) {
-            System.out.println("Autumn");
+        if (a > b) {
+            System.out.print(b + " " + a);
         } else {
-            System.out.println("Winter");
+            System.out.print(a + " " + b);
         }
+
+
     }
 }
