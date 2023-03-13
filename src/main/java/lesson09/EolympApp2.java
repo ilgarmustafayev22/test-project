a0a532c04230f41ee0661892844b090dd8952e47
ilgar222 <ilqar6117@gmail.com>
2023-03-13 23:29:38 +0400

    Task

--- src/main/java/lesson09/EolympApp2.java
+++ src/main/java/lesson09/EolympApp2.java
@@ -5,33 +5,16 @@ import java.util.Scanner;
 public class EolympApp2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
+        int month = scan.nextInt();
 
-        if (num == 1) {
-            System.out.println("January");
-        } else if (num == 2) {
-            System.out.println("February");
-        } else if (num == 3) {
-            System.out.println("March");
-        } else if (num == 4) {
-            System.out.println("April");
-        } else if (num == 5) {
-            System.out.println("May");
-        } else if (num == 6) {
-            System.out.println("June");
-        } else if (num == 7) {
-            System.out.println("July");
-        } else if (num == 8) {
-            System.out.println("August");
-        } else if (num == 9) {
-            System.out.println("September");
-        } else if (num == 10) {
-            System.out.println("October");
-        } else if (num == 11) {
-            System.out.println("November");
-        } else if (num == 12) {
-            System.out.println("December");
-
+        if (month >= 3 && month <= 5) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month <= 8) {
+            System.out.println("Summer");
+        } else if (month >= 9 && month <= 11) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("Winter");
         }
     }
 }
